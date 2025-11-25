2333eaf5bd1a7c03da190b6ae0fa09062ef06ba6
Samvedha Basireddy <sssb@uw.edu>
2025-11-24 18:04:20 -0800

    Made new file for the live camera feed feature

---
+++ camera.py
@@ -0,0 +1,5 @@
+# This file will contain the code for the live camera feed from the drone's
+# camera. 
+
+# The implementation will use OpenCV and Pillow (PIL), which is used for image 
+# manipulation and conversion.
